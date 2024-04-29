d8198d0ddf3a1e82fffa330edd3d7a825692e8f9
Lumasciangelo <lumasciangelo12@gmail.com>
2024-04-28 23:53:00 -0300

    personas aprobadas y no aprobadas

--- TP1/ej_2B.py
+++ TP1/ej_2B.py
@@ -2,7 +2,51 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-from ej_2_fran import analizar_imagen_rasgos_letras
+# from ej_2_fran import analizar_imagen_rasgos_letras
+
+def analizar_imagen_rasgos_letras(img):
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    # Encontrar contornos en la imagen umbralizada
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Dibujar los contornos en una imagen en blanco
+    contour_img = np.zeros_like(img)
+    cv2.drawContours(contour_img, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
+
+    # Detecta los componentes conectados
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img, 8, cv2.CV_32S)
+    stats = sorted(stats, key=lambda x: x[0])
+
+    # Filtra los componentes cuya área es menor a 50 pixeles
+    stats_filtrado = [s for s in stats if s[-1] < 50]
+
+    umbral_max = 9
+    espacios_entre_letras = 0
+
+    repetidos = []
+
+    for i in range(len(stats_filtrado)-1):
+        # Obtiene las coordenadas x del componente actual y del siguiente
+        x_actual = stats_filtrado[i][0]
+        x_siguiente = stats_filtrado[i + 1][0]
+        # Calcula la distancia horizontal entre los componentes
+        distancia_horizontal = x_siguiente - x_actual
+
+        # Si la distancia horizontal es mayor que cierto umbral intuimos que hay un espacio entre palabras.
+        if distancia_horizontal >= umbral_max:
+            # Incrementa el contador de espacios entre letras
+            espacios_entre_letras += 1
+
+    # Elimina los elementos de "stats_filtrados" que están en "repetidos"
+    stats_filtrado = [arr for arr in stats_filtrado if not any((arr == elem).all() for elem in repetidos)]
+    # if len(stats_filtrado) < 1:
+    #     espacios_entre_letras = -1
+    salida = {
+        "Caracteres": len(stats_filtrado),
+        "Espacios": espacios_entre_letras,
+        "Palabras": espacios_entre_letras + 1
+    }
+
+    return salida
 
 examenes = ["TP1/multiple_choice_1.png",
             "TP1/multiple_choice_2.png",
@@ -42,6 +86,7 @@ image = np.ones((500, 400), dtype=np.uint8) * 255
 y = 40
 x = 40
 
+
 for nombre in nombres:
     # nombre = nombres[0]
     nombre.shape
@@ -51,16 +96,21 @@ for nombre in nombres:
     font = cv2.FONT_HERSHEY_SIMPLEX
     tamanio_fuente = 0.5
     color = (0, 0)  # Color en formato BGR (azul, verde, rojo)
+    
 
     # Escribir el texto en la imagen compuesta
-    imagen_con_texto = cv2.putText(image, "Aprobo", (y+250, x+20), font, tamanio_fuente, color, thickness=2)
+    imagen_con_texto = cv2.putText(image, 'Aprobado', (y+250, x+20), font, tamanio_fuente, color, thickness=2)
 
     x += 100
     # Mostrar la imagen
 plt.imshow(image, cmap='gray')
 plt.show()
 
-
+# 1- APROBADO
+# 2- APROBADO
+# 3- NO APROBADO
+# 4- NO APROBADO
+# 5- NO APROBADO
 
 
 
@@ -89,10 +139,6 @@ plt.show()
     #     print("fecha: Mal")
     # else:
     #     print("fecha: Ok")
-
-
-
-    
 
 
     # print("EXAMEN:", examen)
